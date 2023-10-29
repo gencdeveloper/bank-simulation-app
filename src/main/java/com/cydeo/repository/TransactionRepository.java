54eaeeeeb867941f98ea627ac680eb0d9fdce30d
Emre <yunusozudogru34@gmail.com>
2023-10-29 15:23:57 +0100

    TransactionServiceImpl totaly completed.

--- src/main/java/com/cydeo/repository/TransactionRepository.java
+++ src/main/java/com/cydeo/repository/TransactionRepository.java
@@ -16,4 +16,8 @@ public class TransactionRepository {
         transactionList.add(transaction);
         return transaction;
     }
+
+    public List<Transaction> findAll() {
+        return transactionList;
+    }
 }
--- src/main/java/com/cydeo/service/impl/TransactionServiceImpl.java
+++ src/main/java/com/cydeo/service/impl/TransactionServiceImpl.java
@@ -121,6 +121,6 @@ public class TransactionServiceImpl implements TransactionService {
 
     @Override
     public List<Transaction> findAllTransaction() {
-        return null;
+        return transactionRepository.findAll();
     }
 }
