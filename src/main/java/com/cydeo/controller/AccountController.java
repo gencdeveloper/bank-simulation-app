70a5965ef5078691f25dea6798d6b521947c7a1f
Emre <yunusozudogru34@gmail.com>
2023-10-30 15:27:19 +0100

    Post Method

--- src/main/java/com/cydeo/controller/AccountController.java
+++ src/main/java/com/cydeo/controller/AccountController.java
@@ -1,9 +1,15 @@
 package com.cydeo.controller;
 
+import com.cydeo.enums.AccountType;
+import com.cydeo.model.Account;
 import com.cydeo.service.AccountService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.Date;
 
 @Controller
 public class AccountController {
@@ -25,8 +31,25 @@ public class AccountController {
     }
 
     @GetMapping("/create-account")
-    public String getCreateForm() {
+    public String getCreateForm(Model model) {
+
+        //we need to provide empty account object.
+        model.addAttribute("account", Account.builder().build()); // it will create empty account object
+
+        //we need to provide accountType enum info filling the dropdown options.
+        model.addAttribute("accountTypes", AccountType.values());
+
 
         return "account/create-account"; // This should match the name of your HTML template.
+    }
+
+    @PostMapping("/create") //create a method to capture information from ui
+    public String createAccount(@ModelAttribute("account") Account account) {
+
+        //trigger createNewAccount method, create the account based on the user
+            accountService.createNewAccount(account.getBalance(),new Date(),account.getAccountType(),account.getUserId());
+
+        // once user created return back to the index page.
+        return "redirect:/index";
     }
 }
