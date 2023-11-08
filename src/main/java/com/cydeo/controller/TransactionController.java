1dbe2cccae74849b121cca339b167ebf34099fe9
Emre <yunusozudogru34@gmail.com>
2023-11-08 13:47:03 +0100

    added validation

--- src/main/java/com/cydeo/controller/TransactionController.java
+++ src/main/java/com/cydeo/controller/TransactionController.java
@@ -6,11 +6,13 @@ import com.cydeo.service.AccountService;
 import com.cydeo.service.TransactionService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import javax.validation.Valid;
 import java.util.Date;
 import java.util.UUID;
 
@@ -44,7 +46,13 @@ public class TransactionController {
     }
 
     @PostMapping("/transfer")
-    public String makeTransaction(@ModelAttribute("transaction") Transaction transaction) {
+    public String makeTransaction(@Valid @ModelAttribute("transaction") Transaction transaction, BindingResult bindingResult,Model model) {
+
+        if(bindingResult.hasErrors()){
+            model.addAttribute("accounts",accountService.listAllAccount());
+            model.addAttribute("lastTransactions", transactionService.last10Transactions());
+            return "transaction/make-transfer";
+        }
 
         //I have UUID of accounts but i need to provide Account object.
         //I need to find the Accounts based on the ID that I have and use as a parameter complete makeTransfer method.
